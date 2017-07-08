6dc09e743f888e1c7aff319110261802642f2fa8
bvluong <bryantvinluong@gmail.com>
2017-07-07 17:11:44 -0700

    Add npm packages and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+var plugins = [];
+var devPlugins = [];
+var webpack = require("webpack");
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/index.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    },
+    {
+      test: /\.node$/,
+      loader: "node-loader"
+    }
+    ]
+  }
+};
